e716269f6b9a3f26ddffa109f4fcc6e32369d487
kevanmoothien <kevan.moothien@spoonconsulting.com>
2019-01-03 19:17:56 +0400

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
-express()
+let sharinpix = require('sharinpix')
+
+const express = require("express"),
+  app = express(),
+  upload = require("express-fileupload"),
+  csvtojson = require("csvtojson");
+
+  fs = require('fs');
+  
+app.use(upload())
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .post('/fileupload', function (req, res, next) {
+    csvData = req.files['filetoupload'].data.toString('utf8');
+    return csvtojson().fromString(csvData).then(json => {
+      json.forEach((item)=>{
+        sharinpix.import(item.url, item.album_id, {uuid: item.uuid}).then( function(imp){
+          console.log(imp.id)
+        })
+      })
+      res.send('processed')
+      })
+    })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
